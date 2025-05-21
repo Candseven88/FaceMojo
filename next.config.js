acbbaf9f66ca667aa7d4354bc60775210bcc4f22
Candseven88 <wowsuperth@outlook.com>
2025-05-21 08:55:26 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['sharp'],
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '30mb',
+    },
+    responseLimit: false,
+  },
 };
 
 module.exports = nextConfig; 
